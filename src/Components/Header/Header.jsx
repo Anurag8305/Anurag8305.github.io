924a34f09a8da19a47084e6308dba20fb126a266
Anurag Raj <anuragraj8305@gmail.com>
2023-01-10 09:11:07 +0530

    SOme minor changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,29 @@ import "./Header.css";
 import CTA from "./CTA";
 import ME from "../../Images/skills1.png";
 import HeaderSocials from "./HeaderSocials";
-import {CiLocationOn} from "react-icons/ci"
+import { CiLocationOn } from "react-icons/ci";
+import MovingText from "react-moving-text";
 const Header = () => {
 	return (
 		<header>
 			<div className="container header_container">
 				<h5>Hello I'm</h5>
 				<h1>Anurag Raj</h1>
-				<h5 >Full Stack Web Developer</h5>
-				<h6><CiLocationOn />Ranchi, Jharkhand</h6>
+				<MovingText
+					type="popIn"
+					duration="2000ms"
+					delay="2s"
+					direction="alternate-reverse"
+					timing="linear"
+					iteration="infinite"
+					fillMode="none"
+				>
+					<h5>Full Stack Web Developer</h5>
+				</MovingText>
+				<h6>
+					<CiLocationOn />
+					Ranchi, Jharkhand
+				</h6>
 				<CTA />
 				<HeaderSocials />
 				<div className="me">
