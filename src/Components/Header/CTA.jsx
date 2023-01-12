df9774f3c3979cc9a4a7459333a0fd1b73ac6437
Anurag Raj <anuragraj8305@gmail.com>
2023-01-12 22:59:56 +0530

    Soem minor changes

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import CV from "../../Images/Anurag_Raj_Resume.pdf"
+import React from "react";
+import CV from "../../Images/Anurag_Raj_Resume.pdf";
 const CTA = () => {
-  return (
-    <div className='cta'>
-    <a href={CV} download className='btn'>Download CV</a>
-    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-    </div>
-  )
-}
+	return (
+		<div className="cta">
+			<a href={CV} download target="_blank" className="btn">
+				Download CV
+			</a>
+			<a href="#contact" className="btn btn-primary">
+				Let's Talk
+			</a>
+		</div>
+	);
+};
 
-export default CTA
+export default CTA;
